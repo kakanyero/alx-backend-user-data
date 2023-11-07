443c89ee1ffbc27f4d2b2d840eaf45b466d24361
kakanyero <kakanyero142@gmail.com>
2023-11-07 13:31:01 +0300

    first commit

---
+++ 0x01-Basic_authentication/models/user.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" User module
+"""
+import hashlib
+from models.base import Base
+
+
+class User(Base):
+    """ User class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email')
+        self._password = kwargs.get('_password')
+        self.first_name = kwargs.get('first_name')
+        self.last_name = kwargs.get('last_name')
+
+    @property
+    def password(self) -> str:
+        """ Getter of the password
+        """
+        return self._password
+
+    @password.setter
+    def password(self, pwd: str):
+        """ Setter of a new password: encrypt in SHA256
+        """
+        if pwd is None or type(pwd) is not str:
+            self._password = None
+        else:
+            self._password = hashlib.sha256(pwd.encode()).hexdigest().lower()
+
+    def is_valid_password(self, pwd: str) -> bool:
+        """ Validate a password
+        """
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.password is None:
+            return False
+        pwd_e = pwd.encode()
+        return hashlib.sha256(pwd_e).hexdigest().lower() == self.password
+
+    def display_name(self) -> str:
+        """ Display User name based on email/first_name/last_name
+
